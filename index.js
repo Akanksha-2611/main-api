5efd5b4d0cd4ba33565a0bccca5fa0b0fd5b98e4
Akanksha-2611 <akankshatiwari5171@gmail.com>
2024-03-22 21:18:04 +0530

    changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from  'express'
+import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import cors from 'cors'
@@ -6,23 +6,18 @@ import cookieParser from 'cookie-parser'
 dotenv.config()
 import { UserRouter } from './routes/user.js'
 
-const app=express()
+const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:["http://localhost:5173"],
-    credentials:true
+    // origin: ["http://localhost:5173"],
+    origin:[process.env.BASE_URL],
+    credentials: true
 }))
 app.use(cookieParser());
-app.use('/auth',UserRouter)
+app.use('/auth', UserRouter)
 
-mongoose.connect('mongodb://127.0.0.1:27017/authentication')
-
-app.get("/",(req,res,next)=>{
-    return res.status(200).json({
-        success:true,
-        message:"Hello World",
-    });
-})
-app.listen(process.env.PORT,()=>{
+// mongoose.connect('mongodb://127.0.0.1:27017/authentication')
+mongoose.connect(process.env.DATABASE);
+app.listen(process.env.PORT, () => {
     console.log("Server is Running")
 })
